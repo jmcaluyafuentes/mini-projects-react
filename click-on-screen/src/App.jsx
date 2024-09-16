09b101a27737793095792e84e70583a144b7d7d2
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-16 20:25:18 +1000

    feat: add undo functionality to handle undo button click

--- click-on-screen/src/App.jsx
+++ click-on-screen/src/App.jsx
@@ -4,22 +4,32 @@ import './App.css'
 const App = () => {
   const [dots, setDots] = useState([]);
   const [nextId, setNextId] = useState(1);
+  const [cache, setCache] = useState([]);
 
   const clickScreen = ({ clientX: x, clientY: y}) => {
-    const newDot = { nextId, x, y };
+    const newDot = { id: nextId, x, y };
     setDots([...dots, newDot]);
     setNextId(nextId + 1);
+  }
+
+  const undo = () => {
+    if (dots.length === 0) return;
+    const lastDot = dots[dots.length-1];
+    const updatedDots = dots.slice(0, -1);
+
+    setDots(updatedDots);
+    setCache([...cache, lastDot]);
   }
 
   return (
     <div>
       <div className="buttons-container">
-        <button>Undo</button>
+        <button onClick={undo}>Undo</button>
         <button>Redo</button>
       </div>
       <div className="click-screen" onClick={clickScreen}>
-        {dots.map(({nextId, x, y}) => (
-          <div key={nextId} style={{ left: x, top: y }} className="dot">{console.log(nextId)}</div>
+        {dots.map(({id, x, y}) => (
+          <div key={id} style={{ left: x, top: y }} className="dot"></div>
         ))}
       </div>
     </div>
